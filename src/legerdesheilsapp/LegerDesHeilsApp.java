ce7d5d95dac07a7ad6891e644423c31ed43335f4
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-05-18 14:13:07 +0200

    Initial Commit: Parameters retrieved from XML file

---
+++ src/legerdesheilsapp/LegerDesHeilsApp.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package legerdesheilsapp;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.*;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+
+/**
+ *
+ * @author User
+ */
+public class LegerDesHeilsApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // get parameters
+        Document document = getParameters();
+        String conn = document.getElementsByTagName("ConnectString").item(0).getTextContent();
+        String usr = document.getElementsByTagName("Username").item(0).getTextContent();
+        String pwd = document.getElementsByTagName("Password").item(0).getTextContent();
+        
+        
+    }
+     
+    /**
+     * Source: https://stackoverflow.com/a/14968272
+     * 
+     * Returns document containing DB parameters 
+     * from the XML file
+     * 
+     * @return Document
+     */
+    private static Document getParameters() throws ParserConfigurationException, SAXException, IOException {
+        File parameters = new File("src\\legerdesheilsapp\\DBParameters.xml");
+        DocumentBuilderFactory documentBuilderFactory = 
+                DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = 
+                documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(parameters);
+        
+        return document;
+    }
+}
