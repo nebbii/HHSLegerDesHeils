a86b9605970993d1dfa73180aa7b1f4069731ae2
nebbii <ben.wolthuis@copiatek.nl>
2018-06-13 21:41:54 +0200

    [bug] attempt to make buttons functional

--- src/app/Frame.java
+++ src/app/Frame.java
@@ -37,6 +37,7 @@ import org.xml.sax.SAXException;
 public class Frame {
     private Handler handler;
     private JSplitPane mainFrame;
+    private JTable mainTable;
     
     public Frame() throws Exception {
         
@@ -63,10 +64,12 @@ public class Frame {
         JPanel OptionList = this.getQueryButtons();
         
         // Right Main area
-        JTable MainTable = this.getQueryToTable(handler.getUitDienstResult());
+        JTable mainTable = this.getQueryToTable(handler.getUitDienstResult());
+        
+        mainTable = this.getQueryToTable(handler.getInADnotInClever());
         
         mainFrame = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
-                OptionList, MainTable);
+                OptionList, mainTable);
                 
         jf.add(mainFrame);
         jf.setVisible(true);
@@ -82,6 +85,47 @@ public class Frame {
         buttons[1] = new JButton("getInProfitNotInAD");
         buttons[2] = new JButton("getInADNotInProfit");
         buttons[3] = new JButton("getInADnotInClever");
+        
+        class ClickListener1 implements ActionListener {
+             @Override
+             public void actionPerformed(ActionEvent event) {
+                 mainTable = getQueryToTable(handler.getUitDienstResult());
+                 createMainView();
+             }
+        }
+        
+        class ClickListener2 implements ActionListener {
+             @Override
+             public void actionPerformed(ActionEvent event) {
+                 mainTable = getQueryToTable(handler.getInProfitNotInAD());
+                 createMainView();
+             }
+        }
+        
+        class ClickListener3 implements ActionListener {
+             @Override
+             public void actionPerformed(ActionEvent event) {
+                 mainTable = getQueryToTable(handler.getInADNotInProfit());
+                 createMainView();
+             }
+        }
+        
+        class ClickListener4 implements ActionListener {
+             @Override
+             public void actionPerformed(ActionEvent event) {
+                 mainTable = getQueryToTable(handler.getInADnotInClever());
+                 createMainView();
+             }
+        }
+        
+        ActionListener cl1 = new ClickListener1();
+        ActionListener cl2 = new ClickListener2();
+        ActionListener cl3 = new ClickListener3();
+        ActionListener cl4 = new ClickListener4();
+        buttons[0].addActionListener(cl1);
+        buttons[1].addActionListener(cl2);
+        buttons[2].addActionListener(cl3);
+        buttons[3].addActionListener(cl4);
         
         for (JButton i : buttons) {
             if(i!=null) {
@@ -130,12 +174,16 @@ public class Frame {
                     
                     while (rs.next()) {
                         System.out.print("Record found: ");
-                        for (int i = 0; i < rsmd.getColumnCount(); i++) {
-                            data[index][i] = rs.getString(i+1);
-                            System.out.print(rs.getString(i+1));
+                        if(index < 999) {
+                            for (int i = 0; i < rsmd.getColumnCount(); i++) {
+                                data[index][i] = rs.getString(i+1);
+
+                                System.out.print(rs.getString(i+1));
+                                System.out.print(" ");
+                            }
+                            System.out.println("");
+                            index++;
                         }
-                        System.out.println("");
-                        index++;
                     }
                 }
             } catch (SQLException e) {
